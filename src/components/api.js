5db8d8aba31095e43b9c7cb58c3457bea98490de
erak <erik@zirkular.io>
2021-12-17 15:25:55 +0100

    Removes API proxy calls.

--- src/components/api.js
+++ src/components/api.js
@@ -1,24 +1,7 @@
 /* global fetch */
 
-const ApiPath = '/vimeo/api'
-const ConfigApiPath = '/vimeo/config-api'
-
 /** A static class that interfaces with the server-side Vimeo API */
 export default class API {
-  /**
-   * A util method to modify the endpoint to and return a full API request url
-   * @param {string} endpoint - The endpoint you would like to add the full request URL (e.g /videos/video-id)
-   * @returns {string}
-   */
-  static path (endpoint) {
-    // return `${ApiPath}?path=${endpoint}?fields=uri,play,width,height,live,description,title,stats,link`
-    return `${ApiPath}?path=${endpoint}`
-  }
-
-  static configPath (endpoint) {
-    return `${ConfigApiPath}?path=${endpoint}`
-  }
-
   /**
    * A method for requesting Vimeo videos by video id
    * @param {number} videoId - The Vimeo video id you would like to query (e.g 296928206)
@@ -26,7 +9,7 @@ export default class API {
    */
   static getVideo (videoId) {
     return new Promise((resolve, reject) => {
-      fetch(API.path(`/videos/${videoId}`)).then(res => {
+      fetch(`/videos/${videoId}`).then(res => {
         API.sendResponse(res, resolve, reject)
       })
     })
@@ -47,7 +30,7 @@ export default class API {
    */
   static getAlbumVideos (albumId) {
     return new Promise((resolve, reject) => {
-      fetch(API.path(`/albums/${albumId}/videos`)).then(res => {
+      fetch(`/albums/${albumId}/videos`).then(res => {
         API.sendResponse(res, resolve, reject)
       })
     })
